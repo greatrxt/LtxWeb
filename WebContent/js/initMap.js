a99dc618f218bc86243114f69bb6d2da7467b31b
qubit_dev1 <rakshit@1qubit.com>
2016-07-30 19:06:24 +0530

    Minor improvements

--- WebContent/js/initMap.js
+++ WebContent/js/initMap.js
@@ -1,18 +1,18 @@
-var showingRawPoints = true;
-var showingSnappedPoints = true;
+var showingRawPoints = false;
+var showingSnappedPoints = false;
 var showingOsrmPath = true;
-var showingGooglePath = true;
+var showingGooglePath = false;
 var path;
 var snappedPath;
 var osrmPath;
 var googlePath;
 var map;
-
+var mapBoundingDone;
 var osrmLatLngArray = new Array();
 var googleLatLngArray = new Array();
-
+var osrmLimit = 90;
 var vehicleBounds;
-
+var osrmRequestCounter, osrmResponseCounter, osrmFinalResponseIndex;
 /**
  * Loads data from status endpoint
  * @param map
@@ -21,9 +21,19 @@ var vehicleBounds;
 function loadDataOnMap(map) {
   //Loading vehicle information - refer http://www.w3schools.com/json/json_http.asp
   this.map = map;
-  var url = "http://localhost:8080/AngelTwo/rest/status/vehicle/100/1000";
-  
+  mapBoundingDone = false;
+  fetchVehicleData();
   setInterval(function(){
+	  fetchVehicleData();
+  }, 20000);
+}
+
+/**
+ * Fetches vehicle data from server
+ * @returns
+ */
+function fetchVehicleData(){
+	  var url = "http://localhost:8080/AngelTwo/rest/status/vehicle/100/1000";
 	  var xmlhttp = new XMLHttpRequest();
 
 	  xmlhttp.onreadystatechange = function() {
@@ -41,8 +51,6 @@ function loadDataOnMap(map) {
 	  };
 	  xmlhttp.open("GET", url, true);
 	  xmlhttp.send();
-
-  }, 10000);
 }
 
 /**
@@ -53,7 +61,7 @@ function loadDataOnMap(map) {
 function snapLatLngToRoadUsingGoogleRoadsApi(response) {
   var locationArray = response.location;
   
-  var googleRoadApiLimit = 100;
+  var googleRoadApiLimit = 90;
   var begin = 0;
   var end = googleRoadApiLimit;
   if(locationArray.length < googleRoadApiLimit){
@@ -81,7 +89,7 @@ function snapLatLngToRoadUsingGoogleRoadsApi(response) {
 	        	   hideGoogleRouteButtonIfQuotaExceeded();
 	           }
 	       };
-	       xmlhttp.open("GET", url, true);
+	       xmlhttp.open("GET", url, false);//false - calling synchronously now. Causes UI to freeze for a few milliseconds. Needs to be resolved.
 	       xmlhttp.send();
 	  }
 	  
@@ -101,7 +109,10 @@ function snapLatLngToRoadUsingGoogleRoadsApi(response) {
 }
 
 function hideGoogleRouteButtonIfQuotaExceeded(){
-	document.getElementById('googleRouteButton').style.visible = 'hidden';
+	//document.getElementById('googleRouteButton').style.visible = 'hidden';
+	var googleRouteButton = document.getElementById("googleRouteButton");
+	googleRouteButton.value = "Google Route unavailable";
+	
 }
 /**
  * Snap lat lng received to road using OSRM server
@@ -111,17 +122,21 @@ function hideGoogleRouteButtonIfQuotaExceeded(){
 
 function snapLatLngToRoadUsingOsrmRoadsApi(response) {
   var locationArray = response.location;  
-  var osrmLimit = 100;
+  
   var begin = 0;
   var end = osrmLimit;
+  var allCoordinatesAdded = false;
   if(locationArray.length <= osrmLimit){
 	  end = locationArray.length;
+	  allCoordinatesAdded = true;
   }
   
   osrmLatLngArray.length = 0; //clear old data
-  var allCoordinatesAdded = false;
+  
+  osrmRequestCounter = 0;
+  osrmResponseCounter = 0;
+  osrmFinalResponseIndex = -1;
   while(begin < locationArray.length){
-	  
 	  var url = "http://127.0.0.1:5000/match/v1/driving/";
 	  
 	  for(var l = begin; l < end; l++){
@@ -132,30 +147,31 @@ function snapLatLngToRoadUsingOsrmRoadsApi(response) {
 	  }
 	 
 	  
-	  makeRequestAndProcessOsrmResponse = function(url, callback, allCoordinatesAdded){
+	  makeRequestAndProcessOsrmResponse = function(url, callback, allCoordinatesAdded, osrmRequestCounter){
 		  var xmlhttp = new XMLHttpRequest();
 	       xmlhttp.onreadystatechange = function() {
 	           if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-	             callback(xmlhttp.responseText, allCoordinatesAdded);   
+	             callback(xmlhttp.responseText, allCoordinatesAdded, osrmRequestCounter);   
 	           }
 	       };
-	       xmlhttp.open("GET", url, true);
+	       xmlhttp.open("GET", url, false);//false - calling synchronously now. Causes UI to freeze for a few milliseconds. Needs to be resolved.
 	       xmlhttp.send();
 	  }
 	  
 	  begin = end;
 	  end = end + osrmLimit;
+	  makeRequestAndProcessOsrmResponse(url, drawSimplePolylineWithOsrmSnappedPoints, allCoordinatesAdded, osrmRequestCounter);
 	  if(end >= locationArray.length){
 		  end = locationArray.length;
 		  allCoordinatesAdded = true;
 	  } 
 	  
-	  if(begin!=locationArray.length){
+/*	  if(begin!=locationArray.length){
 		  begin = begin -1;
-	  }
+	  }*/
 	  
-	  makeRequestAndProcessOsrmResponse(url, drawSimplePolylineWithOsrmSnappedPoints, allCoordinatesAdded);  
-
+	    
+	  osrmRequestCounter++;
   }
 }
 
@@ -202,6 +218,9 @@ function drawSimplePolylineWithRawPoints(response){
 	  path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
   };
   
+  if(path!=null){
+	  path.setMap(null);
+  }
   path = new google.maps.Polyline({
 	  path:latLngArray,
 	  strokeColor:"#FF0000",
@@ -213,8 +232,20 @@ function drawSimplePolylineWithRawPoints(response){
             offset: '100%'}]
 	  });
   
-  path.setMap(map);
+  if(showingRawPoints){
+	  showingRawPoints = false;
+	  toggleRawPoints();
+	  path.setMap(map);
+  } else {
+	  showingRawPoints = true;
+	  toggleRawPoints();
+	  path.setMap(null);
+  }
+  //path.setMap(map);
   
+  if(snappedPath!=null){
+	  snappedPath.setMap(null);
+  }
   snappedPath = new google.maps.Polyline({
 	  path:snappedLatLngArray,
 	  strokeColor:"#0000FF",
@@ -226,10 +257,24 @@ function drawSimplePolylineWithRawPoints(response){
             offset: '100%'}]
 	  });
   
-  snappedPath.setMap(map);
-  map.fitBounds(vehicleBounds);
-  toggleRawPoints();
-  toggleSnappedPoints();
+  
+  if(showingSnappedPoints){
+	  showingSnappedPoints = false;
+	  toggleSnappedPoints();
+	  snappedPath.setMap(map);
+  } else {
+	  showingSnappedPoints = true;
+	  toggleSnappedPoints();
+	  snappedPath.setMap(null);
+  }
+  
+  //snappedPath.setMap(map);
+  if(!mapBoundingDone){
+	  map.fitBounds(vehicleBounds);
+	  mapBoundingDone = true;
+  }
+//  toggleRawPoints();
+//  toggleSnappedPoints();
 
 }  
 
@@ -238,25 +283,34 @@ function drawSimplePolylineWithRawPoints(response){
  * @param snappedResponse
  * @returns
  */
-function drawSimplePolylineWithOsrmSnappedPoints(snappedResponse, allCoordinatesAdded){
+function drawSimplePolylineWithOsrmSnappedPoints(snappedResponse, allCoordinatesAdded, requestIndex){
   var snappedPointsJson = JSON.parse(snappedResponse).tracepoints;
   
   for(var l = 0; l < snappedPointsJson.length; l++){
 	  if(snappedPointsJson[l]!=null){
 		  var locationJson = snappedPointsJson[l].location;
-		  //osrmLatLngArray.push(new google.maps.LatLng(locationJson.latitude, locationJson.longitude));
-		  osrmLatLngArray.push(new google.maps.LatLng(locationJson[1], locationJson[0]));
+		  var insertIndex = (requestIndex * osrmLimit) + snappedPointsJson[l].waypoint_index;
+		  //osrmLatLngArray.push(new google.maps.LatLng(locationJson[1], locationJson[0]));
+		  osrmLatLngArray.splice(insertIndex, 0, new google.maps.LatLng(locationJson[1], locationJson[0]));
 	  }
   }
   if(allCoordinatesAdded){
+	  osrmFinalResponseIndex = requestIndex;
+  }
+  
+  if(osrmResponseCounter == osrmFinalResponseIndex){
 	  var osrmArrow = {
 		        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
 	  };
 	  
-	  osrmPath.setMap(null);
+	  
+	  if(osrmPath!=null){
+		  osrmPath.setMap(null);
+	  }
 	  osrmPath = new google.maps.Polyline({
 		  path:osrmLatLngArray,
 		  strokeColor:"#008000",
+		  //strokeColor:'#'+(Math.random(requestIndex)*0xFFFFFF<<0).toString(16),
 		  strokeOpacity:0.8,
 		  strokeWeight:4,
 		  icons: [{
@@ -264,10 +318,19 @@ function drawSimplePolylineWithOsrmSnappedPoints(snappedResponse, allCoordinates
 	            repeat:'300px',
 	            offset: '100%'}]
 		  });
-	  osrmPath.setMap(map);	 
-	  //showingOsrmPath = false;
-	  toggleCalculatedRoute();
+
+	  
+	  if(showingOsrmPath){
+		  showingOsrmPath = false;
+		  toggleCalculatedRoute();
+		  osrmPath.setMap(map);
+	  } else {
+		  showingOsrmPath = true;
+		  toggleCalculatedRoute();
+		  osrmPath.setMap(null);
+	  }
   }
+  osrmResponseCounter++;
 }
 
 /**
@@ -286,7 +349,9 @@ function drawSimplePolylineWithGoogleApiSnappedPoints(googleResponse, allCoordin
 	  var googleRouteArrow = {
 		        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
 	  };
-	  googlePath.setMap(null);;
+	  if(googlePath!=null){
+		  googlePath.setMap(null);
+	  }
 	  googlePath = new google.maps.Polyline({
 		  path:googleLatLngArray,
 		  strokeColor:"#0F0F00",
@@ -298,8 +363,15 @@ function drawSimplePolylineWithGoogleApiSnappedPoints(googleResponse, allCoordin
 	            offset: '100%'}]
 		  });
 	  googlePath.setMap(map);	
-	  showingGooglePath = false;
-	  toggleGoogleRoute();
+	  if(showingGooglePath){
+		  showingGooglePath = false;
+		  toggleGoogleRoute();
+		  googlePath.setMap(map);
+	  } else {
+		  showingGooglePath = true;
+		  toggleGoogleRoute();
+		  googlePath.setMap(null);
+	  }
   }
 }
 
